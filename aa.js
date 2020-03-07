c88c6ac8d5b47dcb3593def516f74e0ce8f02fcb
bhaskar29090 <reddybhaskar437@gmail.com>
2020-03-07 04:31:57 -0600

    added more files

---
+++ aa.js
@@ -0,0 +1,8 @@
+
+function Conversion() {
+    const F=document.getElementById('Ki').value;
+    document.getElementById('mycell').value= F*2.20462262185
+};
+
+
+
--- java.js
+++
@@ -1,7 +0,0 @@
-
-<script>
-function temperatureConverter(valNum) {
-  valNum = parseFloat(valNum);
-  document.getElementById("kelvin").innerHTML=(valNum+273.15);
-}
-</script>
