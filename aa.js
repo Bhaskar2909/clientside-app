bf2605454a9db97cd015eb1533b3a70c9c9ebe1e
bhaskar29090 <reddybhaskar437@gmail.com>
2020-04-09 22:02:54 -0500

    commit

--- aa.js
+++ aa.js
@@ -1,8 +1,22 @@
 
+
+const ki = document.getElementById('Ki');
+console.log(ki);
+const mycell = document.getElementById('mycell');
+
+
 function Conversion() {
-    const F=document.getElementById('Ki').value;
-    document.getElementById('mycell').value= F*2.20462262185
+    const F=ki.value;
+    mycell.value= F*2.20462262185;
+    localStorage.setItem("Kilogram",ki.value);
+    localStorage.setItem("Pounds",mycell.value);
 };
 
+function msg(){
+    var x=document.getElementById("gmail");
+    if(x.value==""){
+        alert("fill the box");
 
+    }
+}
 
